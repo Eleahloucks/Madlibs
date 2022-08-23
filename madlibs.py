d52e266f51282cf1861268b1a3ca7322bd0f4595
eleahmloucks <eleahloucks@gmail.com>
2022-08-23 17:56:19 -0600

    added template files

--- madlibs.py
+++ madlibs.py
@@ -50,6 +50,31 @@ def greet_person():
 
     return render_template("compliment.html", person=player, compliment=compliment)
 
+@app.route("/game")
+def show_madlib_form():
+    user_choice = request.args.get('game')
+
+    if user_choice == 'yes':
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+    input_person = request.args.get("madlib-person")
+    input_color = request.args.get("madlib-colors")
+    input_noun = request.args.get("madlib-noun")
+    input_adj = request.args.get("madlib-adjective")
+
+    return render_template("madlib.html", person=input_person, color=input_color, noun=input_noun, adjective=input_adj)
+
+
+    #return render_template("madlib.html", person_choice=input_person, color_choice=input_color, noun_choice=input_noun, adj_choice=input_adj)
+
+# @app.route("/goodbye")
+# def say_goodbye():
+#     return render_template("goodbye.html")
+
 
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
